fd88ca943f9b638bb990cb31ca67a1a5a2ac71e3
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-03-18 14:07:00 -0300

    add: novas pastas para coloboradores

--- app/index.php
+++ app/index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Menu</h1>
+    <h1>BEM-VINDO COLABORADOR  <br> Login</h1>
     
         <form action="verificarlogin.php" method="post">
             
@@ -15,7 +15,7 @@
             <input type="text" name="login" /><br/>
             
             Senha:<br/>
-            <input type="password" name="senha"/><br/>
+            <input type="password" name="senha"/><br/> <br/>
             
             <input type="submit" value="Login"/>
             
