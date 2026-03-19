f28a109f7b0d1d54ff72897da0c9d10615069b84
Luiz Miguel <luizmigueldasilvagomes@gmail.com>
2026-03-19 14:57:17 -0300

    add:new features

--- app/colaborador/menu_adm.php
+++ app/colaborador/menu_adm.php
@@ -3,14 +3,16 @@ include './validar.php'
 
 ?>
 
-<a href="aColaborador.php">Cadastrar colaborador</a>
+<a href="gColaborador.php">Cadastrar colaborador</a>
 <br>
 
 <a href="reserva/index.php">Cadastrar Reserva</a>
 <br>
 
-<a href="reserva/cReserva">Alterações</a>
+<a href="reserva/aReserva">Alterações Reserva</a>
 
+<br>
+<a href="">Alterações Cliente</a>
 <br>
 
 <a href="">Sair</a>
